d520ca086b7daf30195fb0a50a5065c83d968a8c
Abilash2025 <abilash.r2004@gmail.com>
2026-05-26 19:25:22 +0530

    Restructed the test-mages path, added 'Download a Sample MRI' functionality

--- frontend/components/uploadSection.jsx
+++ frontend/components/uploadSection.jsx
@@ -4,7 +4,7 @@ function UploadSection({
 }) {
 
   return (
-
+    <div>
     <div
       {...getRootProps()}
       className="
@@ -42,6 +42,71 @@ function UploadSection({
       </p>
 
     </div>
+    <div
+        className="
+        mt-4
+
+        w-full
+        max-w-3xl
+
+        bg-zinc-900/60
+
+        border
+        border-zinc-800
+
+        rounded-xl
+
+        p-4
+
+        flex
+        flex-col
+        items-center
+        justify-center
+
+        text-center
+    "
+      >
+
+        <p
+          className="
+            text-sm
+            text-zinc-400
+        "
+        >
+          No MRI scan available?
+          Download a sample MRI image to test the application.
+        </p>
+
+        <a
+          href="/test-images/sample-mri.jpg"
+          download
+
+          className="
+            inline-flex
+            items-center
+    
+            mt-3
+
+            px-4
+            py-2
+
+            rounded-lg
+
+            bg-zinc-600
+            hover:bg-zinc-800
+
+            text-white
+            text-sm
+
+            transition
+        "
+        >
+          Download Sample MRI
+        </a>
+
+      </div>
+      </div>
+
   );
 }
 
